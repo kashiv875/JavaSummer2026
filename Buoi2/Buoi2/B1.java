e5c5fe9764424eb219110eb88b9a17e7c1ec52ba
kashiv875 <tungduongcgh123@gmail.com>
2026-07-20 07:58:51 +0700

    Implement multiplication table in B1.java
    Add multiplication table implementation in B1.java

--- Buoi2/Buoi2/B1.java
+++ Buoi2/Buoi2/B1.java
@@ -1,1 +1,14 @@
+package Buoi2;
 
+public class B1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 2; i <= 9; i++) {
+		    for (int j = 1; j <= 10; j++) {
+		        System.out.println(i + " x " + j + " = " + (i * j));
+		    }
+		    System.out.println();
+		}
+	}
+}
