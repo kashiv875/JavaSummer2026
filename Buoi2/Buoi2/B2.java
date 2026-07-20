b76f8ac1e644432682fb2023e097db292e0531bd
kashiv875 <tungduongcgh123@gmail.com>
2026-07-20 07:59:12 +0700

    Add files via upload

---
+++ Buoi2/Buoi2/B2.java
@@ -0,0 +1,20 @@
+package Buoi2;
+
+public class B2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Day binh phuong:   ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i*i + ", ");
+        }
+        System.out.println();
+
+        System.out.print("Day lap phuong:    ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i*i*i + ", ");
+        }
+        System.out.println();
+	}
+
+}
